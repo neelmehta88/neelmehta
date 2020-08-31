8a0e55b088e104a9b55221524598e233e7972dea
neelmehta88 <69890028+neelmehta88@users.noreply.github.com>
2020-08-31 09:51:48 +0530

    Add files via upload

---
+++ calc1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculator using  buttons</title>
+</head>
+<body>
+	<form>
+		<table border="5" cellpadding="5" cellspacing="5" align="center">
+			<tr>
+				<td colspan="5"><input type="text" name="input" readonly="readonly"></td>
+			</tr>
+			<tr>
+				<td align="center"><button>7</button></td>
+				<td align="center"><button>8</button></td>
+				<td align="center"><button>9</button></td>
+				<td align="center"><button>%</button></td>
+				<td align="center"><button>MU</button></td>
+			</tr>
+
+			<tr>
+				<td align="center"><button>4</button></td>
+				<td align="center"><button>5</button></td>
+				<td align="center"><button>6</button></td>
+				<td align="center"><button>*</button></td>
+				<td align="center"><button>/</button></td>
+			</tr>
+
+			<tr>
+				<td align="center"><button>1</button></td>
+				<td align="center"><button>2</button></td>
+				<td align="center"><button>3</button></td>
+				<td rowspan="2" align="center"><button>+</button></td>
+				<td align="center"><button>-</button></td>
+			</tr>
+
+			<tr>
+				<td align="center"><button>0</button></td>
+				<td align="center"><button>00</button></td>
+				<td align="center"><button>.</button></td>
+				<td align="center"><button>=</button></td>
+				
+			</tr>
+			
+
+			
+
+		</table>
+	</form>
+</body>
+</html>
